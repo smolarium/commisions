091b675ba17808361d0d6c6148eb4e7d18a4bd57
Artur Smolarski <artur.smolarski@pb.com>
2022-01-13 14:54:07 +0100

    Add example test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Smolarium\Commissions\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample() : void
+    {
+        $this->markTestIncomplete('This is just the example.');
+    }
+}
